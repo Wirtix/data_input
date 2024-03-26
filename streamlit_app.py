2deb343527e9a812f21161541648c1fcc1bbc62e
Wirtix <163426241+Wirtix@users.noreply.github.com>
2024-03-26 15:22:56 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -92,7 +92,6 @@ elif st.session_state["authentication_status"]:
                 # Update Google Sheets with the new vendor data
                 conn.update(worksheet="Vendors", data=updated_df)
                 conn.update(worksheet="backup", data=updated_df)
-                st.session_state.something = st.session_state.widget
-                st.session_state.widget = ''
+                st.text_input(label="Ulica*") = ''
                 st.success("Poprawnie przesłane Dane!")
 
